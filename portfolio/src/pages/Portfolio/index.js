69db09eb350805ee881cdd4e6e189776c6c0aad0
James Jordan <jpjordan1987@gmail.com>
2021-04-15 23:32:51 -0400

    started with hookRouter, about to try

--- portfolio/src/pages/Portfolio/index.js
+++ portfolio/src/pages/Portfolio/index.js
@@ -1,11 +1,15 @@
-import React from "react"; //, { useState, useEffect } 
+import React from "react"; 
+
+//, { useState, useEffect } 
 // import API from "../../utils/API";
+
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import Project from "../../components/Project";
 import About from "../../components/About";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Contact from "../../components/Contact";
+
 // import Container from "../../components/Container";
 // import SearchForm from "../../components/SearchForm";
 // import SearchResults from "../../components/SearchResults";
