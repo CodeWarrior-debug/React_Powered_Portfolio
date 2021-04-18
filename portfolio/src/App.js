e647d7a540b3fb24734a3caf5fa39431cf9ac7cc
James Jordan <jpjordan1987@gmail.com>
2021-04-18 15:59:49 -0400

    routes working

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,19 +1,26 @@
-import './App.css';
-import Portfolio from './pages/Portfolio';
-import Projects from './pages/Projects';
-import './index.css';
-// import './lux.css';
+import React from "react"; 
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "./lux.css";
+import "./index.css";
+import Portfolio from "./pages/Portfolio";
+import Projects from "./pages/Projects";
+
 
 function App() {
-  return (
-    <div className="App">
-      {/* < Portfolio /> */}
-      < Projects />
-    </div>
-  );
+    return(
+        <Router>
+            <div>
+                <Switch>
+                    <Route exact path="/" component={Portfolio}/>
+                    <Route exact path="/projects" component={Projects}/>
+                    <Portfolio></Portfolio>
+                    <Projects></Projects>
+                </Switch>
+            </div>
+        </Router>
+
+    )
 }
 
 export default App;
-
-
--- portfolio/src/pages/Portfolio/index.js
+++ portfolio/src/pages/Portfolio/index.js
@@ -1,29 +1,21 @@
 import React from "react"; 
-
-//, { useState, useEffect } 
-// import API from "../../utils/API";
-
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
-import Project from "../../components/Project";
 import About from "../../components/About";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Contact from "../../components/Contact";
 
-// import Container from "../../components/Container";
-// import SearchForm from "../../components/SearchForm";
-// import SearchResults from "../../components/SearchResults";
-// import Alert from "../../components/Alert";
 
 function Portfolio(props) {
   return (
+
+    
     <div>
     <Header/>
     <About/>
     <Contact/>
-    <Project/>
     <Footer/>
     </div>
+
   );
 }
 
